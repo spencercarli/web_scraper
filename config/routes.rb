b6c8626aff56ef4102c85ab6e3c55e9aaea161b3
Spencer Carli <spencercarli@gmail.com>
2013-05-13 19:45:33 -0400

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ WebScraper::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'link_finders#index'
 
   # See how all your routes lay out with "rake routes"
 
